29488a94c0829719a568e7586df945def31f0399
Kasper Wepack <kasper@wepack.se>
2025-04-30 16:02:50 +0200

    GetXlsProductsUpdate

--- src/Domain/Entities/Products/Product.cs
+++ src/Domain/Entities/Products/Product.cs
@@ -15,6 +15,21 @@ public class Product
     
     [Required]
     public string Sku { get; set; }
+    
+    // Optionals
+    public string? SupplierSku { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? EAN { get; set; }
+    public string? CategoryId { get; set; }
+    public string? Series { get; set; }
+    public string? Color { get; set; }
+    public string? Material { get; set; }
+    public int? Price { get; set; }
+    public float? Cost { get; set; }
+    public int? Qty { get; set; }
+    public float? Weight { get; set; }
+    public string? MainImage { get; set; }
 
     // Relationsship
     // Has one weeklist
