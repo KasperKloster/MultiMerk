5de34c467298d19e1006a71a9a12805e5404fc60
Kasper Kloster <kasperkloster@gmail.com>
2025-05-05 23:03:43 +0200

    ShippingNumberToWeeklist

---
+++ src/Infrastructure/Migrations/20250505205756_ShippingNumberToWeeklistFurther.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ShippingNumberToWeeklistFurther : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Weeklists",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ShippingNumber",
+                value: "Shipment101");
+
+            migrationBuilder.UpdateData(
+                table: "Weeklists",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ShippingNumber",
+                value: "Shipment102");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Weeklists",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ShippingNumber",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Weeklists",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ShippingNumber",
+                value: "");
+        }
+    }
+}
